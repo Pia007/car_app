7de14e1de91d73d36527fa7189e5e518a933eecc
Pia <piatorain@yahoo.com>
2024-01-13 22:17:00 -0800

    car docstrings and messages

--- app/cars/models.py
+++ app/cars/models.py
@@ -7,6 +7,37 @@ import string
 import random
 
 class Car(models.Model):
+    """
+    Model for Car instances.
+    
+    A Car instance represents a car in the inventory. It contains information
+    about the car, including the make, model, year, color, price, mileage,
+    whether or not it is sold and a vehicle identification number. If it is sold, the date sold and the salesperson who sold it are also included.
+    
+    Attributes:
+        make: A string representing the make of the car.
+        model: A string representing the model of the car.
+        year: An integer representing the year the car was made.
+        color: A string representing the color of the car.
+        price: An integer representing the price of the car.
+        mileage: An integer representing the mileage of the car.
+        sold: A boolean representing whether or not the car is sold.
+        date_sold: A date representing the date the car was sold.
+        salesperson: A Salesperson instance representing the salesperson who sold
+            the car.
+        vin: A string representing the VIN of the car.
+        
+    Methods:
+        calculate_commission: Calculates the commission for the car.
+        formatted_mileage: Returns the mileage of the car as a string with
+            thousands separators.
+        formatted_price: Returns the price of the car as a string with thousands
+            separators.
+        mark_as_not_sold: Marks the car as not sold.
+        clean: Validates the Car instance.
+        save: Saves the Car instance.
+    """
+
     make = models.CharField(max_length=50, blank=False, null=False)
     model = models.CharField(max_length=50, blank=False, null=False)
     year = models.IntegerField(blank=False, null=False)
@@ -16,9 +47,9 @@ class Car(models.Model):
     sold = models.BooleanField(default=False)
     date_sold = models.DateField(blank=True, null=True)
     salesperson = models.ForeignKey('salespeople.Salespeople', on_delete=models.SET_NULL, null=True, blank=True, related_name='sold_cars')
-    vin = models.CharField(max_length=17, unique=True, blank=True)  # New field for VIN
+    vin = models.CharField(max_length=17, unique=True, blank=True) 
 
-    # New field for car type
+    """ Car Type """
     CONVERTIBLE = 'Convertible'
     COUPE = 'Coupe'
     HATCHBACK = 'Hatchback'
@@ -39,34 +70,117 @@ class Car(models.Model):
 
     COMMISSION_RATE = 0.20 # 20% commission rate
 
-
     def calculate_commission(self):
+        """ 
+        Calculates the commission for the car.
+
+        Args:
+            self: An instance of the Car class.
+            
+        Returns:
+            A float representing the commission for the car.
+        """
         commission =  self.price * self.COMMISSION_RATE
         return round(commission, 2)
 
     def _generate_vin(self):
         """
-        Generate a random 17-character VIN.
-        Note: This is a simplified example and doesn't follow the actual VIN standards.
+        Generate a random 17-character VIN (Vehicle Identification Number).
+
+        This method generates a random 17-character string that simulates a VIN, which is used to uniquely
+        identify vehicles. Please note that this is a simplified example for demonstration purposes
+        and doesn't adhere to actual VIN standards.
+
+        Args:
+            self: An instance of the Car class.
+
+        Returns:
+            vin: A string representing the VIN of the car.
         """
         return ''.join(random.choices(string.ascii_uppercase + string.digits, k=17))
     
     def formatted_mileage(self):
-        locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')  # Set locale, e.g., 'en_US.UTF-8'
+        """
+        Formats the mileage of the car to include thousands separators.
+
+        This method uses the locale module to format the car's mileage attribute to a string based on the locale of the user. The mileage integer is initially formatted as a decimal number, using %d. Then the decimal is set to include thousands separators, using the grouping=True parameter. The decimal number is then formated to a string.
+        
+        Args:
+            self: An instance of the Car class.
+
+        Returns:
+            mileage(str): The car's mileage with thousands separators.
+        """
+        locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')  
         return locale.format_string("%d", self.mileage, grouping=True)
     
     def formatted_price(self):
+        """
+        Formats the price of the car to include thousands separators.
+        
+        This method uses the locale module to format the car's price attribute to a string based on the locale of the user. The price integer is initially formatted as a decimal number, using %d. Then the decimal is set to include thousands separators, using the grouping=True parameter. The decimal number is then formated to a string.
+        
+        Args:
+            self: An instance of the Car class.
+            
+        Returns:
+            price(str): The car's price with thousands separators.
+        """
         locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')  # Set locale, e.g., 'en_US.UTF-8'
         return locale.format_string("%d", self.price, grouping=True)
     
     def mark_as_not_sold(self):
+        """
+        Sets the sold attribute to false.
+        
+        This methods sets a previously sold car's sold attribute to false, sets the salesperson attribute to None and saves the car instance when the user marks the car as not sold.
+        
+        Args:
+            self: An instance of the Car class.
+            
+        Returns:
+            None
+        """
         self.sold = False
         self.salesperson = None  # Set the salesperson to None
         self.save()
 
     def clean(self):
-        # Validation logic
-        #check if car make, model, year, year and mileage is alread in a car , if it is then raise validation error
+        """
+        Performs data validation for the Car instance.
+
+        This method is used to validate the attributes of a Car instance before saving it to the database.
+        It checks for various validation rules and raises ValidationError if any of the rules are violated.
+
+        Validation Rules:
+        1. Car Existence Check:
+            - Checks if a car with the same make, model, year, color, mileage, and car_type already exists in the database.
+            - Uses iexact operator to make a case-insensitive comparison of the make, model, and color.
+            - If a duplicate car is found (excluding the current instance by its ID), it raises a ValidationError.
+
+        2. Sold Car Validation:
+            - If the 'sold' attribute is True, it checks for the presence of 'date_sold' and 'salesperson'.
+            - If 'date_sold' or 'salesperson' is missing when a car is marked as sold, it raises a ValidationError.
+
+        3. Salesperson Assignment Validation:
+            - If a 'salesperson' is assigned to the car, it checks for the presence of 'date_sold' and 'sold' status.
+            - If 'date_sold' or 'sold' status is missing when a salesperson is assigned, it raises a ValidationError.
+
+        4. Sale Date Validation:
+            - If a 'date_sold' is specified, it checks for the presence of 'salesperson' and 'sold' status.
+            - If 'salesperson' or 'sold' status is missing when a sale date is set, it raises a ValidationError.
+
+        Args:
+            self: An instance of the Car class.
+
+        Raises:
+            ValidationError: If any of the validation rules are violated, a ValidationError is raised
+            with an appropriate error message.
+
+        Note:
+            This method is typically called automatically when saving a Car instance using Django's ModelForm,
+            ensuring that the data conforms to the specified validation rules before it is persisted in the database.
+        """
         if self.make and self.model and self.year and self.color and self.mileage:
             if Car.objects.filter(make__iexact=self.make, model__iexact=self.model, year=self.year, color__iexact=self.color, mileage=self.mileage, car_type=self.car_type).exclude(id=self.id).exists():
                 raise ValidationError("Car already exists")
@@ -81,6 +195,27 @@ class Car(models.Model):
             raise ValidationError("Sold status and Salesperson are required when a sale date is set.")
     
     def save(self, *args, **kwargs):
+        """
+        Saves the Car instance to the database.
+
+        This method is used to save the Car instance to the database. It follows these steps:
+        1. Validates the Car instance using the clean() method to ensure data integrity.
+        2. Generates a Vehicle Identification Number (VIN) for the car if it doesn't already have one.
+        3. Capitalizes the 'make' and 'model' fields for consistency.
+        4. Saves the Car instance to the database by calling the save() method on the superclass and   passing in *args, **kwargs to ensure that the custom logic and default saving behavior are applied when saving a Car instance.
+
+        Args:
+            self: An instance of the Car class.
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Note:
+            - The clean() method is called to validate the Car instance before saving.
+            - If the 'vin' field is empty, a VIN is generated.
+            - The 'make' and 'model' fields are capitalized for consistency.
+            - Finally, the Car instance is saved to the database.
+
+        """
         self.clean()
         # Generate VIN only if it doesn't exist
         if not self.vin:
@@ -88,14 +223,15 @@ class Car(models.Model):
 
         for field_name in ['make', 'model', 'color']:
             val = getattr(self, field_name, False)
-            # if make is BMW make it all uppercase
+            # accounts for BMW, GMC, etc.
             if len(val) == 3:
                 setattr(self, field_name, val.upper())
             else:
                 setattr(self, field_name, val.capitalize())
 
-        # Save the Car instance first to get an ID
+        
         super(Car, self).save(*args, **kwargs)
 
     def __str__(self):
+        """ Returns a string representation of the Car instance. """
         return f'{self.vin} - {self.year} {self.make} {self.model} - {self.car_type}'
--- app/cars/views.py
+++ app/cars/views.py
@@ -2,7 +2,7 @@ from django import forms
 from django.shortcuts import get_object_or_404, redirect
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib import messages
 from .models import Car
@@ -10,29 +10,81 @@ from salespeople.models import Salespeople
 from django.db.models import Count, Sum, Value, DecimalField
 
 class CarForm(forms.ModelForm):
+    """     
+    Form for creating and updating Car instances.
+
+    This form is used to gather data for a Car object, including the associated
+    salesperson. The salesperson field is optional and can be dynamically updated 
+    on the client side using JavaScript.
+    """
     salesperson = forms.ModelChoiceField(
         queryset=Salespeople.objects.all(),
         required=False,  # Make it optional
         widget=forms.Select(attrs={'onchange': 'updateSalespersonDetails();'})  
     )
     class Meta:
+        """
+        Meta class for CarForm.
+
+        Specifies the model to which the form is linked and the fields that are
+        included in the form.
+        """
         model = Car
         fields = [
-            'make', 'model', 'year', 'color', 'price', 'mileage', 'sold', 'date_sold',
-            'car_type', 'salesperson'  # Include the salesperson field
+            'id', 'make', 'model', 'year', 'color', 'price', 'mileage', 'sold', 'date_sold',
+            'car_type', 'salesperson'
         ]
+class HomePageView(TemplateView):
+    """
+    Displays the main landing page for the Car app.
 
-class CarIndexView(ListView):
-    model = Car
+    This view is the home page for the Car app. This class extends Django's generic class-based view, TemplateView, to render a static home page.
+
+    Attributes:
+        - `template_name`: The associated template 'cars/index.html' is used for rendering the home page.
+
+    The resulting home page will display relevant information or content related to the Car app.
+    """
     template_name = 'cars/index.html'
-    context_object_name = 'cars'
-
+    
 class CarListView(ListView):
+    """     
+    Displays a list of Car instances with filtering and ordering options.
+    
+    This class utilizes the Django generic class-based view, ListView, to create a view to handle the list of car objects from a queryset. It defines the model, template, and context_object_name attributes. A template associated with 'cars/list.html' is used to render with a list of car objects. The context_object_name attribute specifies the name of the context variable that will be used in the template. By default, the context variable is named after the model, in lowercase. In this case, the context variable will be named 'cars'.
+
+    Attributes:
+        - `model`: The model associated with this view is the Car model.
+        - `template_name`: The associated template 'cars/list.html' is used for rendering the list of cars.
+        - `context_object_name`: The context variable name is 'cars'.
+
+    Methods:
+        - `get_queryset()`: Returns a filtered and ordered queryset of Car instances.
+        - `get_context_data()`: Adds filter options to the context.
+    """
     model = Car
     template_name = 'cars/car_list.html'
     context_object_name = 'cars'
 
     def get_queryset(self):
+        """     
+        Returns a filtered and ordered queryset of Car instances.
+        
+        This method overrides the default get_queryset() method of the ListView class. It returns a filtered and ordered queryset of Car instances based on the query parameters in the request. 
+        
+        The queryset is filtered by the following query parameters:
+            - `color_filter`: The color of the car.
+            - `make_filter`: The make of the car.
+            - `model_filter`: The model of the car.
+            - `year_filter`: The year of the car.
+            - `car_type_filter`: The type of the car.
+
+        The queryset is ordered by the following query parameters:
+            - `order`: The field to order by. The default ordering is by id.
+            - `price_filter`: The price of the car.
+            - `mileage_filter`: The mileage of the car.
+        
+        """
         queryset = super().get_queryset()
         order = self.request.GET.get('order')
         color_filter = self.request.GET.get('color_filter') 
@@ -40,9 +92,7 @@ class CarListView(ListView):
         model_filter = self.request.GET.get('model_filter')
         year_filter = self.request.GET.get('year_filter')
         car_type_filter = self.request.GET.get('car_type_filter')
-        price_filter = self.request.GET.get('price_filter')
 
-        # Apply color filter if a color is selected
         if color_filter:
             queryset = queryset.filter(color=color_filter)
 
@@ -58,27 +108,46 @@ class CarListView(ListView):
         if car_type_filter:
             queryset = queryset.filter(car_type=car_type_filter)
         
-        if price_filter:
-            queryset = queryset.filter(price=price_filter)
-            
-        #order by mileage without decimals
         if order in ['mileage', '-mileage']:
-            # Annotate with total sales, defaulting to 0.00 if there are no sales
+            """
+            Annotates the queryset with total mileage and orders it by mileage.
+
+            The sum of 'mileage' values is calculated for Car instances in the queryset. If the car has no mileage, the value is defaulted to 0.00. The data type of the field is a decimal number. Then the sum is annotated to each car as 'total_sales_mileage'. The list of cars can then be ordered by mileage, either in ascending or descending order.
+
+            Parameters:
+                - order (str): The sorting order, either 'mileage' (ascending) or '-mileage' (descending).
+            """
             queryset = queryset.annotate(
                 total_sales_mileage=Sum('mileage', default=Value(0.00), output_field=DecimalField())
             ).order_by(order)
 
         if order in ['price', '-price']:
-            # Annotate with total sales, defaulting to 0.00 if there are no sales
+            """
+            Annotates the queryset with total price and orders it by price.
+            
+            The sum of 'price' values is calculated for Car instances and each car is annotated with 'total_sales_price'. If the price is not available, the value is defaulted to 0.00. The data type of the field is a decimal number. The list of cars can then be ordered by price, either in ascending or descending order.
+            
+            Parameters:
+                - order (str): The sorting order, either 'price' (ascending) or '-price' (descending).
+            """
             queryset = queryset.annotate(
                 total_sales_price=Sum('price', default=Value(0.00), output_field=DecimalField())
             ).order_by(order)
             
-
-    
         return queryset
     
-    def get_context_data(self, **kwargs):   
+    def get_context_data(self, **kwargs):
+        """     
+        Adds filter options to the context.
+
+        This method retrieves distinct values for various car attributes (makes, models, colors, years, and car types) and adds them to the context dictionary. These values are used to provide filter options on the webpage.
+        
+        Args:
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            dict: A dictionary containing filter options for makes, models, colors, years, and car types added to the context.
+        """
         context = super().get_context_data(**kwargs)
         
         makes = Car.objects.values_list('make', flat=True).distinct()
@@ -96,18 +165,53 @@ class CarListView(ListView):
         return context
 
 class CarDetailView(DetailView):
+    """     
+    Displays the details of a Car instance.
+    
+    This class utilizes the Django generic class-based view, DetailView, to create a view to handle the details of a car object from a queryset. It defines the model, template, and context_object_name attributes. A template associated with 'cars/detail.html' is used to render with a car object. The context_object_name attribute specifies the name of the context variable that will be used in the template. By default, the context variable is named after the model, in lowercase. In this case, the context variable will be named 'car'.
+    
+    Attributes:
+        - `model`: The model associated with this view is the Car model.
+        - `template_name`: The associated template 'cars/detail.html' is used for rendering the details of a car.
+        - `context_object_name`: The context variable name is 'car'.
+    """
     model = Car
     template_name = 'cars/car_detail.html'
     context_object_name = 'car'
 
 @method_decorator(csrf_exempt, name='dispatch')
 class CarCreateView(CreateView):
+    """
+    Creates a Car instance.
+    
+    This class utilizes the Django generic class-based view, CreateView, to create a view to handle the creation of a car object. It defines the model, template, form_class, and success_url attributes. A template associated with 'cars/car_form.html' is used to create a car object. The form_class attribute specifies the form to use for creating a car object. The success_url attribute specifies the URL to redirect to after a successful form submission. The decorator is used to exempt the view from the CSRF token requirement.
+    
+    Attributes:
+        - `model`: The model associated with this view is the Car model.
+        - `template_name`: The associated template 'cars/car_form.html' is used for rendering the form for creating a car.
+        - `form_class`: The form used for creating a car is the CarForm.
+        - `success_url`: The URL to redirect to after a successful form submission is the car list page.
+        
+    Methods:
+        - `form_valid()`: Handles the form submission when creating a new Car instance.
+    """
     model = Car
     form_class = CarForm  # Use the modified CarForm
     template_name = 'cars/car_form.html'
     success_url = reverse_lazy('car_list')
 
     def form_valid(self, form):
+        """     
+        Handles the form submission when creating a new Car instance.
+        
+        This method overrides the default form_valid() method of the CreateView class. It handles the form submission when creating a new Car instance. If there is a salesperson selected, first name and last name values are automatically validated by Django. If valid, the data is stored in the cleaned_data dictionary and saved. The Car object is saved.
+        
+        Args:
+            form (CarForm): The form used for creating a car.
+            
+        Returns:
+            HttpResponseRedirect: A redirect to the success URL.
+        """
         # Get the selected salesperson from the form
         selected_salesperson = form.cleaned_data['salesperson']
         
@@ -124,15 +228,47 @@ class CarCreateView(CreateView):
 
 @method_decorator(csrf_exempt, name='dispatch')
 class CarUpdateView(UpdateView):
+    """     
+    Updates an existing Car instance.
+
+    This class utilizes the Django generic class-based view, UpdateView, to create a view
+    for updating an existing Car instance. It defines the model, template, fields, and
+    success_url attributes.
+
+    Attributes:
+        - `model`: The model associated with this view is the Car model.
+        - `template_name`: The associated template 'cars/car_form.html' is used for rendering the form for updating a car.
+        - `fields`: The fields that can be updated in the Car instance. In this case, it includes:
+            'id', 'make', 'model', 'year', 'color', 'price', 'mileage', 'sold', 'date_sold',
+            'salesperson', and 'car_type'.
+        - `success_url`: The URL to redirect to after a successful update, in this case, the car list.
+
+    Methods:
+        - `form_valid()`: Handles the form submission when updating a Car instance.
+    """
     model = Car
     template_name = 'cars/car_form.html'
-    fields = ['make', 'model', 'year', 'color', 'price', 'mileage', 'sold', 'date_sold', 'salesperson', 'car_type']  # Added 'car_type'
+    fields = ['id', 'make', 'model', 'year', 'color', 'price', 'mileage', 'sold', 'date_sold', 'salesperson', 'car_type']  # Added 'car_type'
     success_url = reverse_lazy('car_list')
 
     def form_valid(self, form):
+        """
+        Handles the form submission when updating a Car instance.
+
+        This method overrides the default form_valid() method of the UpdateView class.
+        It handles the form submission when updating a Car instance. If the car is marked
+        as sold and not already marked as sold with a salesperson, it calls the 'mark_as_sold'
+        method to update the car's status. It saves the Car object. 
+
+        Args:
+            form (CarForm): The form used for updating a car.
+
+        Returns:
+            HttpResponseRedirect: A redirect to the success URL
+        """
         car = form.save(commit=False)
 
-        # Check if the car is being marked as sold and not already marked
+        """Check if the car is being marked as sold and not already marked"""
         if car.sold and not car.date_sold and car.salesperson:
             car.mark_as_sold(car.salesperson)
 
@@ -141,17 +277,39 @@ class CarUpdateView(UpdateView):
         return super().form_valid(form)
 
 class CarDeleteView(DeleteView):
+    """
+    Deletes an existing Car instance.
+    
+    This class utilizes the Django generic class-based view, DeleteView, to create a view to handle the deletion of a car object. It defines the model, template, and success_url attributes. A template associated with 'cars/car_confirm_delete.html' is used to confirm the deletion of a car object. The success_url attribute specifies the URL to redirect to after a successful deletion.
+    
+    Attributes:
+        - `model`: The model associated with this view is the Car model.
+        - `template_name`: The associated template 'cars/car_confirm_delete.html' is used for rendering the confirmation page for deleting a car.
+        - `success_url`: The URL to redirect to after a successful deletion is the car list page.
+    """
     model = Car
     template_name = 'cars/car_confirm_delete.html'
     success_url = reverse_lazy('car_list')
 
 def mark_car_as_not_sold(request, car_id):
+    """
+    Sets the sold status of a car to False.
+    
+    This function sets the sold status of a car to False and clears the date_sold and salesperson fields. It is used to mark a car as not sold. It is called when the 'Mark as Not Sold' button is clicked on the salesperson detail page. The try block attempts to save the car instance. If successful, a success message is displayed. Otherwise, an error message is displayed.
+    It redirects back to the salesperson detail page if the car was sold by a salesperson. Otherwise, it redirects back to the car list page.
+    
+    Args:
+        request (HttpRequest): The HTTP request.
+        car_id (int): The id of the car to mark as not sold.
+        
+    Returns:
+        HttpResponseRedirect: A redirect to the car list or salesperson detail page.
+    """
     car = get_object_or_404(Car, pk=car_id)
 
-    # Update the car status
     car.sold = False
-    car.date_sold = None  # Clear the date_sold field
-    car.salesperson = None  # Clear the salesperson field
+    car.date_sold = None  
+    car.salesperson = None 
 
     # Save the car instance
     try:
