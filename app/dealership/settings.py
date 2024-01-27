ebe0aa638df10059888858b223c68c9b7a2fef39
Pia <piatorain@yahoo.com>
2024-01-26 17:35:01 -0800

    layout edits and footer

--- app/dealership/settings.py
+++ app/dealership/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5dp$_f5e!q4&*bh@yi&%g8cp*=3%v)wlyhnf8$8fts5uah@bob'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
